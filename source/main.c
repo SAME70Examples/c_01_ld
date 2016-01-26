7bc767200829862b44eb4da3469c46e84d381a40
Manuel Limones <limonesu.me@gmail.com>
2016-01-26 03:27:19 -0600

    change tabs to spaces

--- source/main.c
+++ source/main.c
@@ -8,14 +8,14 @@ void delay_ms(int time_ms);
 void delay_us(int time_us);
 
 int main(){
-	SystemCoreClockUpdate();
-	led_init();
-	while(1){
-		led_setState(LED_ON);
-		delay_ms(500);
-		led_setState(LED_OFF);
-		delay_ms(500);
-	}
+    SystemCoreClockUpdate();
+    led_init();
+    while(1){
+        led_setState(LED_ON);
+        delay_ms(500);
+        led_setState(LED_OFF);
+        delay_ms(500);
+    }
 }
 
 #define PMC_PCR_CMD_READ (0<<12) 
@@ -25,33 +25,33 @@ int main(){
 #define PMC_PCR_PID_PIOC (12)
 
 /**
-	* Initialize led on PC8
-	*
+    * Initialize led on PC8
+    *
 **/
 void led_init(void){
-	PMC->PMC_PCR = (PMC_PCR_EN)|(PMC_PCR_CMD_WRITE)|(PMC_PCR_PID_PIOC);//Enable PIOC Peripheral
-	PIOC->PIO_PER = PIO_PER_P8;//Enable direct control of PC8
-	PIOC->PIO_MDER = PIO_MDER_P8;//Open drain driver
-	PIOC->PIO_PUDR = PIO_PUDR_P8;//Disable pull ups
-	PIOC->PIO_PPDDR = PIO_PPDDR_P8;//Disable pull downs
-	PIOC->PIO_OER = PIO_OER_P8;//Configure as output
+    PMC->PMC_PCR = (PMC_PCR_EN)|(PMC_PCR_CMD_WRITE)|(PMC_PCR_PID_PIOC);//Enable PIOC Peripheral
+    PIOC->PIO_PER = PIO_PER_P8;//Enable direct control of PC8
+    PIOC->PIO_MDER = PIO_MDER_P8;//Open drain driver
+    PIOC->PIO_PUDR = PIO_PUDR_P8;//Disable pull ups
+    PIOC->PIO_PPDDR = PIO_PPDDR_P8;//Disable pull downs
+    PIOC->PIO_OER = PIO_OER_P8;//Configure as output
 }
 
 void led_setState(int led_state){
-	if(led_state == LED_ON){
-		PIOC->PIO_CODR = PIO_CODR_P8;//Output low: led on
-	}else{
-		PIOC->PIO_SODR = PIO_SODR_P8;//Ouput floating: led off
-	}
+    if(led_state == LED_ON){
+        PIOC->PIO_CODR = PIO_CODR_P8;//Output low: led on
+    }else{
+        PIOC->PIO_SODR = PIO_SODR_P8;//Ouput floating: led off
+    }
 }
 
 void delay_ms(int time_ms){
-	delay_us(time_ms*1000);
+    delay_us(time_ms*1000);
 }
 
 #define DELAY_SCALE_FACTOR_US 2
 void delay_us(int time_us){
-	unsigned int total_time  = time_us * (SystemCoreClock /(1000000 * DELAY_SCALE_FACTOR_US));
-	for(unsigned int i = 0; i< total_time; i++){
-	}
+    unsigned int total_time  = time_us * (SystemCoreClock /(1000000 * DELAY_SCALE_FACTOR_US));
+    for(unsigned int i = 0; i< total_time; i++){
+    }
 }
